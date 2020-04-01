5fb20389322fe860fa1df1b1d59956df5b5bf9f4
Jake Mathews <jake.mathews.email@gmail.com>
2020-04-01 16:14:56 -0400

    fix(signature-scan): Allow user's to use license search option with upload source. (Fixes IDETECT-1894)

--- src/main/java/com/synopsys/integration/detect/tool/signaturescanner/BlackDuckSignatureScanner.java
+++ src/main/java/com/synopsys/integration/detect/tool/signaturescanner/BlackDuckSignatureScanner.java
@@ -38,7 +38,6 @@ import com.synopsys.integration.blackduck.codelocation.signaturescanner.ScanBatc
 import com.synopsys.integration.blackduck.codelocation.signaturescanner.ScanBatchBuilder;
 import com.synopsys.integration.blackduck.codelocation.signaturescanner.ScanBatchOutput;
 import com.synopsys.integration.blackduck.codelocation.signaturescanner.ScanBatchRunner;
-import com.synopsys.integration.blackduck.codelocation.signaturescanner.command.IndividualFileMatching;
 import com.synopsys.integration.blackduck.codelocation.signaturescanner.command.ScanCommandOutput;
 import com.synopsys.integration.blackduck.codelocation.signaturescanner.command.ScanTarget;
 import com.synopsys.integration.blackduck.codelocation.signaturescanner.command.SnippetMatching;
@@ -210,20 +209,24 @@ public class BlackDuckSignatureScanner {
         scanJobBuilder.cleanupOutput(false);
 
         final Optional<SnippetMatching> snippetMatching = signatureScannerOptions.getSnippetMatching();
-        if (signatureScannerOptions.getUploadSource() && !snippetMatching.isPresent() && !signatureScannerOptions.getLicenseSearch()) { // TODO: I think this check needs to be verified - JM
-            throw new DetectUserFriendlyException("You must enable snippet matching using " + DetectProperties.Companion.getDETECT_BLACKDUCK_SIGNATURE_SCANNER_SNIPPET_MATCHING().getName() + " in order to use upload source.",
+        if (Boolean.TRUE.equals(signatureScannerOptions.getUploadSource()) && !snippetMatching.isPresent() && Boolean.FALSE.equals(signatureScannerOptions.getLicenseSearch())) {
+            throw new DetectUserFriendlyException(
+                "You must enable snippet matching using "
+                    + DetectProperties.Companion.getDETECT_BLACKDUCK_SIGNATURE_SCANNER_SNIPPET_MATCHING().getName()
+                    + " or enable license search using "
+                    + DetectProperties.Companion.getDETECT_BLACKDUCK_SIGNATURE_SCANNER_LICENSE_SEARCH().getName()
+                    + " in order to use upload source.",
                 ExitCodeType.FAILURE_CONFIGURATION);
         }
-        scanJobBuilder.uploadSource(snippetMatching.get(), signatureScannerOptions.getUploadSource());
+        snippetMatching.ifPresent(scanJobBuilder::snippetMatching);
+        scanJobBuilder.uploadSource(signatureScannerOptions.getUploadSource());
+        scanJobBuilder.licenseSearch(signatureScannerOptions.getLicenseSearch());
 
         signatureScannerOptions.getAdditionalArguments().ifPresent(scanJobBuilder::additionalScanArguments);
 
         final String projectName = projectNameVersion.getName();
         final String projectVersionName = projectNameVersion.getVersion();
         scanJobBuilder.projectAndVersionNames(projectName, projectVersionName);
-
-        final Boolean licenseSearch = signatureScannerOptions.getLicenseSearch();
-        scanJobBuilder.licenseSearch(licenseSearch);
 
         signatureScannerOptions.getIndividualFileMatching()
             .ifPresent(scanJobBuilder::individualFileMatching);
